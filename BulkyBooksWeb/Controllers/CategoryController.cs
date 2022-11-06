029f679695487fe8a7e1558e9d542d9c96ed8929
Bishal Raj Parajuli <hello76tortugaa@gmail.com>
2022-11-06 20:14:34 +0530

    creation of Database

---
+++ BulkyBooksWeb/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using BulkyBooksWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBooksWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        //GET: HOME
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+        //GET: ADD
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
---
+++ BulkyBooksWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BulkyBooksWeb.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace BulkyBooksWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
