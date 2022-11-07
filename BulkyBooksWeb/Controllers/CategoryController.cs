1a306d371f2fd9e6a74acbeb17557805ef3a542f
Bishal Raj Parajuli <hello76tortugaa@gmail.com>
2022-11-07 14:53:19 +0530

    Basic CRUD Done

--- BulkyBooksWeb/Controllers/CategoryController.cs
+++ BulkyBooksWeb/Controllers/CategoryController.cs
@@ -26,5 +26,77 @@ namespace BulkyBooksWeb.Controllers
         {
             return View();
         }
+
+        //POST: ADD
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Add(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        //GET: EDIT
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id== 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Categories.Find(id);
+
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        //POST: EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        //GET: DELETE
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Categories.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        //POST: DELETE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Remove(obj);
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
     }
 }
